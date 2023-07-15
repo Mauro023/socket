58d63e1915cbbb3a9f968b3372c1ed3b42d2a172
luis miguel yances castillo <luisyances1@gmail.com>
2023-07-14 20:40:10 -0500

    build: consultas de ultimo registro y existencia de registro en la base de datos

--- Anviz/Validations.py
+++ Anviz/Validations.py
@@ -10,7 +10,7 @@ def conexion() -> pymysql.connect:
     :return:
         pymysql.connect: Objeto de conexión a la base de datos
 
-    :raises
+    raises:
         pymysql.err.OperationalError: Si ocurre un error al conectarse a la base de datos
     """
     try:
@@ -25,37 +25,51 @@ def conexion() -> pymysql.connect:
         return None
 
 
-def querys(conector_sql, mode=None,
-           table=None, column=None, target=None) -> None:
+def eloquent(conector_sql, mode=None,
+           target_1=None, target_2=None, target_3=None) -> pymysql.Connection:
     """
-    Ejecuta una consulta SQL en la base de datos utilizando un objeto de conexión.
+    Ejecuta una consulta SQL en la base de datos utilizando un objeto de conexión
+    Dependiendo del modo se debe pasar los parametros en orden
+    para mode: None target_1=id, target_2=workday, target_3=None
+    de lo contrario target_1=workday, target_2=aentrytime, target_3=id
 
+    :param target_3: Tercer parametro para coincidencia, si es necesario.
+    :param target_2: Segunda coincidencia para el where anidado.
+    :param target_1: Primera coincidencia para la cláusula where.
     :param conector_sql: Objeto de conexión a la base de datos.
-    :param mode: Modo de consulta. Valor por defecto es None.
-    :param table: Nombre de la tabla en la base de datos. Valor por defecto es None.
-    :param column: Nombre de la columna en la tabla. Valor por defecto es None.
-    :param target: Valor objetivo para filtrar los resultados de la consulta. Valor por defecto es None.
-    :return: None si la consulta arroja error, de lo contrario todas las filas de la consulta
+    :param mode: Modo de consulta. Unica o anidado Valor por defecto es None.
+    :return: None si la consulta arroja error, de lo contrario todas las filas de la consulta.
 
     """
     cur = conector_sql.cursor()
     if mode is None:
         cur.execute(f""" 
-                    SELECT * FROM {table}
-                    WHERE {table}.{column} = {target}
+                    SELECT * FROM attendances
+                    WHERE attendances.employe_id = {target_1}
+                    AND attendances.workday = '{target_2}'
+                    ORDER BY employe_id DESC;
                     """)
-    if cur:
-        print(f'Existen resultados {type(cur)}')
-        for row in cur:
-            print(row)
-        return cur
+        if cur:
+            return cur.fetchone()
+        else:
+            return None
     else:
-        print('No existen resultados')
-        return None
+        cur.execute(f"""
+                    SELECT * FROM attendances
+                    WHERE attendances.workday = '{target_1}'
+                    AND attendances.aentry_time = '{target_2}'
+                    AND attendances.employe_id = {target_3}""")
+        if cur:
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
-    querys(conexion(), table='attendances', column='employe_id', target=4)
+    lastAttendance = eloquent(conexion(), target_1=3, target_2='2023-04-28')
+    attendanceExists = eloquent(conexion(),mode='exists', target_1='2023-04-28', target_2='15:41:39', target_3=3)
+    print(lastAttendance)
+    print(attendanceExists)
 
 
 
